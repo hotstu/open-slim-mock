1e2c8e762fd32d85bd3cbe472149e5f935c26128
hglf <hgleifeng@foxmail.com>
2019-02-19 21:50:16 +0800

    fix

--- src/router/router.fallback.js
+++ src/router/router.fallback.js
@@ -2,7 +2,7 @@ const Handler = require("../handler");
 
 const fallback = new Handler();
 fallback.registerHandler(/[\s\S]+/, async (match, {res, endWithText})=> {
-    return await endWithText(res, JSON.stringify({"error":"404, router is not configured"}), 404);
+    return await endWithText(JSON.stringify({"error":"404, router is not configured"}), 404);
 });
 
 module.exports = fallback;
